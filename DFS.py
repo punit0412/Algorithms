90ed9c699e4c3e70d1b049ce952ca262cda76973
Punit Mishra <78646645+punit0412@users.noreply.github.com>
2023-08-03 00:58:01 +0530

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -11,14 +11,29 @@ def depth_first_search(start_node):
 
     while stack:
         actual_node = stack.pop()
-        actual_node.visited =True
-
+        actual_node.visited = True
+        print(actual_node.name)
         for n in actual_node.adjacency_list:
             if not n.visited:
                 n.visited = True
-                stack.insert(n)
+                stack.append(n)
 
+if __name__ == '__main__':
+    node1 = Node('d')
+    node2 = Node('e')
+    node3 = Node('g')
+    node4 = Node('f')
+    node5 = Node('r')
+    node6 = Node('s')
 
+    node1.adjacency_list.append(node2)
+    node1.adjacency_list.append(node3)
+    node2.adjacency_list.append(node5)
+    node2.adjacency_list.append(node6)
+    node3.adjacency_list.append(node5)
+    node5.adjacency_list.append(node4)
+
+    depth_first_search(node2)
 
 
 
